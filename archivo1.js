8ff77c034986f979664529801382ed7348c51a87
GinoLucianoRojo1993 <gino.luciano.workcontact@gmail.com>
2020-09-16 21:57:09 -0300

    cancioncita de drexler

--- archivo1.js
+++ archivo1.js
@@ -1,1 +1,2 @@
-console.log("nuevos textos");
+console.log("que viva, la telefonia, en todas sus variantes!");
+console.log ("pensando estaba, que te me escabullias! cuando vi tu nombre en la llamada entrante"!)
